0c1c080e863a21fb96b5a84c857371e5b51aa3f9
TopQuark12 <tat1212wong@gmail.com>
2020-10-18 19:54:40 +0800

    multithreading working somewhat

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,14 +1,36 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+using namespace rtos;
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LEDG, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+void loop()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+}
+
+void test_thread_func()
+{
+    while (1) {
+        digitalWrite(LEDG, HIGH);
+        delay(500);
+        digitalWrite(LEDG, LOW);
+        delay(500);
+    }
+}
+
+int main()
+{
+    setup();
+    Thread test_thread;
+    test_thread.start(&test_thread_func);
+    while (1)
+        loop();
 }
